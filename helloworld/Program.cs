286ac7c316bbcf91261c21127ba5f84b2c1e7983
emreyilmaz <emreyilmazxy@gmail.com>
2025-05-05 16:55:02 +0300

    Add project files.

---
+++ helloworld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace helloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Emre Yılmaz. Ben iş hayatına geç atıldım. İş hayatına atıldığımdan beri, o sevdiğim bir düzeni olan, yaparken sıkılmadığım mesleği arayıp durdum. " +
+     "Kendimi keşfettiğimde, bilgisayar başında çalıştığım işlerde zamanın benim için su gibi aktığını fark ettim ve yazılım alanında, küçüklüğümü düşünüp bu alanda yeteneğim olduğunu hatırladım. " +
+     "Sosyokültürel olarak yüksek insanlarla çalışmak, liyakatın diğer meslek gruplarına göre daha iyi işlediğini bilmek, bir düzen ve mantık barındırması beni bu sektöre olan merakımı daha da ilerletti. " +
+     "Aynı zamanda bir doğa sever olarak, bu meslekte bazen bir nehir kenarında bilgisayarımı açmış çalışıyorken kendimi hayal edebiliyorum.");
+
+           
+        }
+    }
+
+}
